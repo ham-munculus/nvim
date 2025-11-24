20e2f27c14fccd161ce6e422672d2c3f4f8fa62a
ham-munculus <242454600+ham-munculus@users.noreply.github.com>
2025-11-24 12:06:45 -0800

    automated commit from archive

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,30 +1,11 @@
 require("blink-cmp").setup({
-	keymap = {
-		preset = "default",
-		["<CR>"] = { "fallback" }, -- Disable accept, only use normal Enter
-	},
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-	},
-	fuzzy = { implementation = "prefer_rust" },
-	completion = { documentation = { auto_show = false } },
+  keymap = {
+    preset = "default",
+    ["<CR>"] = { "fallback" }, -- Disable accept, only use normal Enter
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+  fuzzy = { implementation = "prefer_rust" },
+  completion = { documentation = { auto_show = false } },
 })
--- return {
---   "saghen/blink.cmp",
---   dependencies = { { "rafamadriz/friendly-snippets" } },
---   version = "1.*",
---   ---@module 'blink.cmp'
---   ---@type blink.cmp.Config
---   opts = {
---     keymap = {
---       preset = "default",
---       ["<CR>"] = { "fallback" }, -- Disable accept, only use normal Enter
---     },
---     sources = {
---       default = { "lsp", "path", "snippets", "buffer" },
---     },
---     fuzzy = { implementation = "prefer_rust_with_warning" },
---     completion = { documentation = { auto_show = false } },
---   },
---   opts_extend = { "sources.default" },
--- }
